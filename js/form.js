4cf9d14fe67d29a57d977432c6225470f7d5b843
Angela Capillo <hello@angelcapillo.com>
2025-03-15 14:37:39 +0100

    Remove script from profile and bookmark, create form.js, move variables to form.js, change script in form.js

---
+++ js/form.js
@@ -0,0 +1,40 @@
+const submitButton = document.querySelector('[data-js="submit-button"]');
+
+submitButton.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+
+    console.log(data);
+
+});
+
+const card = document.createElement("li");
+card.className = "card";
+
+const cardIconButton = document.createElement("button");
+cardIconButton.className = "card__icon-button";
+
+const cardBookmark = document.createElement("img");
+cardBookmark.src = "./assets/icons/bookmark-white.svg";
+
+const cardTextQuestion = document.createElement("p");
+cardTextQuestion.className = "card__text";
+
+const cardButton = document.createElement("button");
+cardButton.className = "card__button";
+
+const cardTextAnswer = document.createElement("p");
+cardTextAnswer.className = "card__text card__text__answer text__hidden";
+
+const cardCategories = document.createElement("ul");
+cardCategories.role = "list";
+cardCategories.className = "card__categories";
+
+const cardTag = document.createElement("li");
+cardTag.className = "card__tag";
+
+cardCategories.append(cardTag);
+cardIconButton.append(cardBookmark);
+
+card.append(cardIconButton, cardTextQuestion, cardButton, cardCategories);
--- js/index.js
+++ js/index.js
@@ -17,36 +17,3 @@ const cardAnswer = document.querySelector('[data-js="card-answer"]');
 showButton.addEventListener("click", () => {
     cardAnswer.classList.toggle("text__hidden");
 });
-
-
-const submitButton = document.querySelector('[]')
-
-const card = document.createElement("li");
-card.className = "card";
-
-const cardIconButton = document.createElement("button");
-cardIconButton.className = "card__icon-button";
-
-const cardBookmark = document.createElement("img");
-cardBookmark.src = "./assets/icons/bookmark-white.svg";
-
-const cardTextQuestion = document.createElement("p");
-cardTextQuestion.className = "card__text";
-
-const cardButton = document.createElement("button");
-cardButton.className = "card__button";
-
-const cardTextAnswer = document.createElement("p");
-cardTextAnswer.className = "card__text card__text__answer text__hidden";
-
-const cardCategories = document.createElement("ul");
-cardCategories.role = "list";
-cardCategories.className = "card__categories";
-
-const cardTag = document.createElement("li");
-cardTag.className = "card__tag";
-
-cardCategories.append(cardTag);
-cardIconButton.append(cardBookmark);
-
-card.append(cardIconButton, cardTextQuestion, cardButton, cardCategories);
